87e5bf53e913df473c629ec74e8c5f420766a0b0
Dina Maslova <dinamamaslova@gmail.com>
2024-07-01 01:48:46 +0200

    styly

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ window.addEventListener("scroll", function () {
   let scrolled = window.scrollY;
   let scrollable = document.documentElement.scrollHeight - window.innerHeight;
 
-  if (Math.ceil(scrolled) > 1000) {
+  if (Math.ceil(scrolled) > 900) {
     let toTop = document.querySelector(".top-page");
     toTop.style.display = "block";
 
